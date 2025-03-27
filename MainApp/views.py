a5d220e7799adcde61b34c19c93aa0ab4e971b96
Gal4en0k <barinovagalina@list.ru>
2025-03-27 15:56:21 +0300

    Spippet edit

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from MainApp.forms import SnippetForm
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
@@ -42,7 +42,41 @@ def snippet_detail(request, snippet_id: int):
         return HttpResponseNotFound(f"Snippet with id={snippet_id} not found")
     else: 
         context ["snippet"] = snippet
-    return render(request, 'pages/snippet_detail.html', context)
+        context ["type"] = "view"
+        return render(request, 'pages/snippet_detail.html', context)
+
+def snippet_delete(request, snippet_id: int):
+    if request.method in ("POST" , "GET"):
+        snippet = get_object_or_404(Snippet, id=snippet_id) 
+        snippet.delete()
+    return redirect("list_snip")
+
+def snippet_edit(request, snippet_id: int):
+    context = {"pagename": 'редактирование сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id) 
+    except ObjectDoesNotExist: 
+        return Http404
+    #variant1
+    # if request.method == "GET":
+    #     form = SnippetForm(instance=snippet)
+    #     return render(request,"pages/add_snippet.html", {"form": form} )
+    
+    #variant2
+    if request.method == "GET":
+        context = {'snippet': snippet,
+                   "type" : "edit"}
+        return render(request, 'pages/snippet_detail.html', context)
+    
+    if request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        # snippet.lang = data_form["lang"]
+        snippet.code = data_form["code"]
+        # snippet.creation_date = data_form["creation_date"]
+        snippet.save()
+        return redirect("list_snip")
+
 
 # def create_snippet(request):
 #     if request.method == "POST":
